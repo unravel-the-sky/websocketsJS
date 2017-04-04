99cf395f9fe1e2a9dc61274366a3b68348a8c0d1
Sadan Ekdemir <sekdemir@slb.com>
2017-04-04 14:58:49 +0200

    update; adding more of it;

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-    res.send('<h1> Hello </h1>');
+    // res.send('<h1> Hello </h1>');
+    res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', (socket) => {
+    console.log('a user is connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
 })
 
 http.listen(3000, () => {
