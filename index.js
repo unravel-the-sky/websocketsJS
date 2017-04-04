92d9249ebbb997560e82f94c79b32601e2ee4bb6
Sadan Ekdemir <sekdemir@slb.com>
2017-04-04 12:49:46 +0200

    new; initial push, learning websockets for js;

---
+++ index.js
@@ -0,0 +1,10 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', (req, res) => {
+    res.send('<h1> Hello </h1>');
+})
+
+http.listen(3000, () => {
+    console.log('listening on *: 3000');
+})
